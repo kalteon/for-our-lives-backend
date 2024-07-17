a917a1162e99712852b00b3bc9e52d239c8018a2
kalteon <102862962+kalteon@users.noreply.github.com>
2024-07-17 14:49:29 +0900

    remove: Person

--- src/main/java/org/example/person/repository/PersonRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.person.repository;
-
-import org.example.person.entity.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonRepository extends JpaRepository<Person, Long> {
-}
